81256a083f641f0cf6c7794dc5a6cacabd1ef738
adamcaak <ladamczak1997@gmail.com>
2025-07-30 20:47:34 +0200

    Initial Commit

---
+++ Scrumdigner/ScrumdignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdignerApp.swift
+//  Scrumdigner
+//
+//  Created by Łukasz Adamczak on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
